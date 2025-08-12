be81ebe1afb0b3617262fd574dcfc192695cb961
Smoresies <gavin.britton@gmail.com>
2025-08-11 20:53:52 -0700

    Some Changes?

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Smoresies
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Smoresies
+
+#pragma once
+
+#include "CoreMinimal.h"
+
