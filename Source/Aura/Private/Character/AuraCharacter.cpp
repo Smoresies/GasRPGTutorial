c49ed84f7b3b9294f80fb164068968f4a1344396
Smoresies <gavin.britton@gmail.com>
2025-08-14 11:32:58 -0700

    Lecture 12 - Hooked movement up, added game mode, fixed animations, added camera

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,19 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// Should be done in editor but default can be set here
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0);
+
+	// Typical for top-down game
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Smoresies
+
+
+#include "Game/AuraGameModeBase.h"
+
