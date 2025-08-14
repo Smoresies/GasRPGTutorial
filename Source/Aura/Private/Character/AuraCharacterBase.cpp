34938b582e666ba77a0098132c60a2584f0efd1c
Smoresies <gavin.britton@gmail.com>
2025-08-13 18:33:49 -0700

    Lecture 7 - Add base Character and Enemy BP

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -10,6 +10,10 @@ AAuraCharacterBase::AAuraCharacterBase()
 	// Currently set to false as this base class will not Tick
 	PrimaryActorTick.bCanEverTick = false;
 
+	// CreateDefaultSubobject ONLY USED in Constructor
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -19,4 +19,10 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// TObjectPtr Notes:
+	// Packaged acts as same as Raw
+	// In Editor allows access-tracking, lazy loading allows not loading until actually needed
+	
+	UPROPERTY(EditAnywhere, Category= "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 };
