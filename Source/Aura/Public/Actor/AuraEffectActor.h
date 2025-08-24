086804a0f5dffc150f4ca33de8ede15405261de5
Smoresies <gavin.britton@gmail.com>
2025-08-23 21:30:40 -0700

    Lecture 39 - Setup Gameplay Effect version of Health Potion and also made a Mana Potion.

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -23,6 +23,6 @@ protected:
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(EditAnywhere, Category="Applied Effects")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 };
